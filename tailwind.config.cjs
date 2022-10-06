440fcf4a6edd3b347ddcee090a8e3fea5047247d
matb85 <mateuszbis85@gmail.com>
2022-10-06 11:51:56 +0200

    feat: :sparkles: add tailwind & a custom font

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      primary: "Prata, serif",
+    },
+  },
+  plugins: [],
+};
