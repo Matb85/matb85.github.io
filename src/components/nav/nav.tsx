ed6fdc460047b6938c27f12ceebe6739cce8539d
matb85 <mateuszbis85@gmail.com>
2022-10-06 17:04:13 +0200

    feat: :sparkles: animate the topbar on scroll

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,14 +1,42 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useClientEffect$, useStore } from "@builder.io/qwik";
 import { Logo } from "../icons/qwik";
 
+export const scroll = (store: { opacity: number; y: number; bg: string }) => {
+  if (window.scrollY > window.innerHeight || window.scrollY < 10) {
+    store.y = 0;
+    store.opacity = 1;
+  } else {
+    store.y = -100;
+    store.opacity = 0;
+  }
+  if (window.scrollY > window.innerHeight) {
+    store.bg = "bg-primary-200";
+  } else {
+    store.bg = "bg-white";
+  }
+};
+
 export default component$(() => {
+  const store = useStore({
+    opacity: 0,
+    y: -100,
+    bg: "bg-primary-200",
+  });
+
+  useClientEffect$(() => {
+    scroll(store);
+    window.addEventListener("scroll", () => scroll(store));
+  });
   return (
-    <section class="fixed top-0 w-full h-20 backdrop-blur cr_flex bg-primary-200 bg-opacity-40 z-50">
+    <section
+      style={"transform: translateY(" + store.y + "%);"}
+      class={"fixed transition-transform top-0 w-full h-20 backdrop-blur cr_flex bg-opacity-40 z-50 " + store.bg}
+    >
       <a href="mailto:hello@mateuszbis.com" class="absolute left-4">
         hello@mateuszbis.com
       </a>
       <Logo className="h-16 w-16 mx-auto" />
-      <a class="absolute right-4" href="#">
+      <a class="absolute right-4 transition-opacity" href="#" style={"opacity: " + store.opacity}>
         back to top
       </a>
     </section>
