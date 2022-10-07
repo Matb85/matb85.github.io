fd9a31ceccc3f4f4703d44662130e0b6ff1b6071
matb85 <mateuszbis85@gmail.com>
2022-10-07 12:24:16 +0200

    chore: :arrow_up: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { basePathname } from "@qwik-city-plan";
 
 export default defineConfig(() => {
   return {
     base: "/portfolio/",
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity({ basePathname: "/portfolio/" }), qwikVite(), tsconfigPaths()],
   };
 });
