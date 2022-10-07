95dae6ff3e67eb7d9330f65b7e3b31a83992a30c
matb85 <mateuszbis85@gmail.com>
2022-10-07 11:17:27 +0200

    feat: :zap: update aboutme text

--- src/components/index/about.tsx
+++ src/components/index/about.tsx
@@ -8,11 +8,16 @@ export default component$(() => {
       <H text={text} />
       <section class="w-full px-4 md:px-20 flex flex-col-reverse lg:flex-row gap-8 justify-center overflow-hidden">
         <p data-aos="fade-right" class="w-full lg:w-1/2 text_border">
-          Hi, My name is Mateusz Bis, welcome to my portfolio! I'm a web developer since 2018, and a Google certified UX
-          designer since 2021. Through my career, I've had a chance to work on a variety of projects, from static
-          websites through backend application to android apps. I love experimenting with designs, which means I often
-          start with a sheet of paper to try out lots of versions. After work, I'm a keen runner, a photography amateur
-          and a coffee lover. I bet our cooperation will be fruitful!
+          Hi, My name is Mateusz Bis, welcome to my portfolio! I'm a web developer since 2018, a Google certified UX
+          designer since 2021 and the founder of
+          <a href="https://redinnlabs.com" target="_blank" class="text-[#4f46e5]">
+            {" "}
+            Redinn Labs web agency
+          </a>
+          . Through my career, I've had a chance to work on a variety of projects, from static websites through backend
+          application to android apps. I love experimenting with designs, which means I often start with a sheet of
+          paper to try out lots of versions. After work, I'm a keen runner, a photography amateur and a coffee lover. I
+          bet our cooperation will be fruitful!
         </p>
         <img data-aos="fade-left" src="/i.svg" class="w-full mx-auto lg:w-1/2 object-contain" alt="interests" />
       </section>
