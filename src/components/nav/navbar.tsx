d77056be369a65cefe274fe7fe34b786b9075874
matb85 <mateuszbis85@gmail.com>
2022-10-09 18:56:17 +0200

    fix: :iphone: fix navbar scrolling bug on mobile

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -1,20 +1,24 @@
-import { component$, useClientEffect$, useRef, useStore } from "@builder.io/qwik";
+import { component$, useClientEffect$, useRef, useStore, $ } from "@builder.io/qwik";
 import { normalise } from "../utils/utils";
-
-export function btnClick(nav: HTMLElement) {
-  if (window.outerWidth > 850) return;
-
-  setTimeout(
-    () => nav.scrollTo({ top: 0, left: (window.scrollY / document.documentElement.scrollHeight) * window.innerWidth }),
-    1000
-  );
-}
 
 export default component$(() => {
   const links = ["Home", "About me", "Selected works", "Other projects", "Contact"];
   const headers: HTMLElement[] = [];
-  const state = useStore({ current: 0, progress: 0 });
+  const state = useStore({ current: 0, progress: 0, oldCounter: 0, canScroll: true });
   const nav = useRef<HTMLElement>();
+
+  const btnClick = $(() => {
+    if (window.outerWidth > 850) return;
+    state.canScroll = false;
+    setTimeout(() => {
+      nav.current!.scrollTo({
+        top: 0,
+        left: (window.scrollY / document.documentElement.scrollHeight) * window.innerWidth,
+      });
+      state.canScroll = true;
+    }, 1000);
+  });
+
   useClientEffect$(() => {
     for (let i = 0; i < links.length; i++) headers[i] = document.getElementById(normalise(links[i])) as HTMLElement;
     /** look out! headers have the .scroll-mt-24 class */
@@ -32,6 +36,14 @@ export default component$(() => {
           state.current = i;
           state.progress =
             100 - ((next_el + scroll_offset - window.scrollY) / (next_el + scroll_offset - headers[i].offsetTop)) * 100;
+
+          if (state.oldCounter != state.current && state.canScroll) {
+            nav.current!.scrollTo({
+              top: 0,
+              left: (window.scrollY / document.documentElement.scrollHeight) * window.innerWidth,
+            });
+            state.oldCounter = state.current;
+          }
         }
       }
     });
@@ -42,7 +54,7 @@ export default component$(() => {
       <nav ref={nav} class="flex backdrop-blur gap-4 p-4 bg-primary-200 bg-opacity-40 max-w-full overflow-auto">
         {links.map((x, i) => (
           <a
-            onClick$={() => btnClick(nav.current as HTMLElement)}
+            onClick$={btnClick}
             href={"#" + normalise(x)}
             class="btn flex-none"
             style={
