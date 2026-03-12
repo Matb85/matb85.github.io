440151ad138539aa6ecf73ef7d53bffb78818677
Matb85 <Matb85@users.noreply.github.com>
2026-03-12 18:27:52 +0000

    deploy: 08745f87008937ba4cac01a929258a02f8593a10

--- _astro/Joke.astro_astro_type_script_index_0_lang.E1Ivn1Oe.js
+++
@@ -1,1 +0,0 @@
-import{f as t}from"./utils.B1jAwrOM.js";const e=document.getElementById("joke-button"),n=document.getElementById("joke-content");e.addEventListener("click",async()=>{n.innerText=await t()});
